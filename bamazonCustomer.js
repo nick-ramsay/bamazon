fe7281dfc55eafc867412e53f530a0c8783ca3a4
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-12 18:42:40 +1100

    Completed initial functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,9 +6,104 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "data.Dump89",
-    database: "top_songs_db"
+    database: "bamazon"
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        throw err
+    }
 });
+
+currentItems = [];
+
+function displayItems() {
+    connection.query("select * from products;", function (err, res) {
+        if (err) {
+            throw err;
+        } else {
+            res.forEach(function (item, index) {
+                currentItems.push(res[index].item_id);
+                var item = "-- Item #: " + res[index].item_id + " --\n";
+                item += "Product: " + res[index].product_name + "\n";
+                item += "Price: $" + res[index].price;
+                console.log(item);
+            });
+            purchase();
+        }
+    })
+};
+
+function purchase() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "number",
+                message: "What item ID would you like to purchase?",
+                validate: function (input) {
+                    if (currentItems.indexOf(input) == -1) {
+                        return "This item ID doesn't exist. Try again.";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                name: "quantity",
+                type: "number",
+                message: "How many units of this item would you like to purchase?"
+            }
+        ]).then(function (answer) {
+            var query = connection.query(
+                "select * from bamazon.products where item_id = " + answer.id + ";",
+                function (err, res) {
+                    var itemResults = res;
+                    if (err) {
+                        throw err;
+                    } else if (answer.quantity > res[0].stock_quantity) {
+                        console.log("Insufficient quantity!\nTry again.");
+                        purchase();
+                    } else {
+                        var newQuantity = res[0].stock_quantity - answer.quantity;
+                        connection.query(
+                            "UPDATE products SET stock_quantity = " + newQuantity + " where item_id = " + answer.id + ";",
+                            function (err, res) {
+                                if (err) {
+                                    throw err;
+                                } else {
+                                    console.log("Thank you for purchasing " + answer.quantity + " unit(s) of " + itemResults[0].product_name + ".\nYour total cost was: $" + (answer.quantity * itemResults[0].price));
+                                }
+                                shopAgain();
+                            });
+                    }
+                })
+        });
+}
+
+function shopAgain() {
+    inquirer
+        .prompt({
+            name: "restart",
+            type: "confirm",
+            message: "Would you like to try another search?",
+            default: true
+        }).then(function (answer) {
+            switch (answer.restart) {
+                case true:
+                    displayItems();
+                    break;
+                case false:
+                    exit();
+                    break;
+            }
+        }
+        )
+};
+
+function exit() {
+    console.log("Goodbye.");
+    connection.end();
+};
+
+displayItems();
